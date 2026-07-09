711599e0aaf57b753bbb7970f5e9d8f8a5d0704e
charles-turner-1 <charles-turner-1@users.noreply.github.com>
2026-07-09 03:53:02 +0000

    deploy: 0a7d87f4462f9ee2b3bc517122bd8655237b648c

---
+++ branches/27-group-experiments/_nuxt/Bljs-Eiu.js
@@ -0,0 +1,1 @@
+import{r as t}from"./BoEqP_iH.js";import a from"./ByGf7am6.js";import{B as e,o as n,m as s,w as c,l as i}from"./CRDuorF6.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,m)=>(n(),s(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
