6d725bc2e4cac208334f67b544208a45bf0809eb
charles-turner-1 <charles-turner-1@users.noreply.github.com>
2026-07-09 06:20:48 +0000

    deploy: afd44f0592665171342a9197d2e3a4de6956c307

---
+++ branches/reorder-items/_nuxt/SZMPTDsz.js
@@ -0,0 +1,1 @@
+import{r as n}from"./JC6Nl6oI.js";import a from"./CwX31MIK.js";import{B as s,o as t,m as e,w as i,l as p}from"./C4gvfQom.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,m)=>(t(),e(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
