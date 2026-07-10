3aebc8f5b3cb4ae2b1a589e5e9c108138c4517ef
charles-turner-1 <charles-turner-1@users.noreply.github.com>
2026-07-10 01:59:06 +0000

    deploy: 9621ff8a1dd14970375943d12f0b5c9ead868080

---
+++ branches/reorg-layout/_nuxt/DA2UyO-j.js
@@ -0,0 +1,1 @@
+import{r as n}from"./BXI7hDyU.js";import a from"./ng8rugaT.js";import{B as s,o as t,m as e,w as i,l as p}from"./BbZ79rWl.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,m)=>(t(),e(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
