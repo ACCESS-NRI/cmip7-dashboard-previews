6e7ced86047efe8e54151efe86ed5ad436a1753c
charles-turner-1 <charles-turner-1@users.noreply.github.com>
2026-07-22 00:58:25 +0000

    deploy: 0a172c8cd7f964f30c49841344f742e141d93d53

---
+++ branches/merge-heros/_nuxt/DXEGYJS3.js
@@ -0,0 +1,1 @@
+import{r as n}from"./BTnXVQNI.js";import a from"./BW3uYbk7.js";import{B as s,o as t,m as e,w as i,l as p}from"./kIP7AK9w.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,m)=>(t(),e(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
