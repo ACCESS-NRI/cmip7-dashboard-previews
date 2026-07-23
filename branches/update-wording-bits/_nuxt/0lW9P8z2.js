646babaa0fdfdec7c86d11ba3b6a62015076e09a
charles-turner-1 <charles-turner-1@users.noreply.github.com>
2026-07-23 00:25:30 +0000

    deploy: abc184d1b1140d165edc85987ee43639e74be537

---
+++ branches/update-wording-bits/_nuxt/0lW9P8z2.js
@@ -0,0 +1,1 @@
+import{r as n}from"./dmF01AA6.js";import a from"./rjGobcrY.js";import{B as s,o as t,m as e,w as i,l as p}from"./E8YEnsSj.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,m)=>(t(),e(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
